6a1b5cdcc1d5a3fbda5257da6bc7f000481148c4
Manish <chandrajob365@gmail.com>
2017-07-18 15:00:05 +0530

    Add check for duplicate click on same userName

--- components/chatComponent.js
+++ components/chatComponent.js
@@ -1,6 +1,7 @@
 import {Component} from 'react'
 import ChatSideBar from '../components/chatSideBar'
 import ChatPannel from '../components/chatPannel'
+import Store from '../globalStore/store'
 class ChatComponent extends Component {
   constructor (props) {
     super(props)
@@ -11,43 +12,50 @@ class ChatComponent extends Component {
     }
   }
   componentDidMount () {
-    console.log('<chatComponent>componentDidMount ********* window ******** ', window)
-    const userData = window.userData
     this.setState({
-      user: Object.assign(this.state.user, userData)
+      user: Object.assign(this.state.user, Store.userData)
     })
-  }
-  componentWillUnmount () {
-    console.log('----- <chatComponent>componentWillUnmount ------- ')
   }
   renderChatPanel (obj) {
-    console.log('renderChatPanel ********* window ******** ', window)
-    console.log('<chatComponent.js renderChatPanel> obj.key = ', obj.key, '  obj.user = ', obj.user, '  obj.user.userName = ', obj.user.userName)
-    console.log('<chatComponent.js renderChatPanel> this.state.users = ', this.state.user)
-
-    window.userData = {
-      [obj.key]: {name: obj.user.userName}
+    if (this.componentAlreadyRendered(obj.userName) === 0) {
+      if (Store.userData)
+        Store.userData = Object.assign(Store.userData, {[obj.key]: obj.userName})
+      else
+        Store.userData = Object.assign({}, {[obj.key]: obj.userName})
+      this.setState({
+        user: Object.assign(this.state.user, Store.userData)
+      })
     }
-    console.log('********** <chatComponent.js renderChatPanel> *******  userData = ', window.userData)
-    this.setState({
-      user: Object.assign(this.state.user, window.userData)
-    })
-    console.log('********** <chatComponent.js renderChatPanel> *******  this.state = ', this.state)
-    console.log('<chatComponent.js renderChatPanel> this.state.user = ', this.state.user)
-    console.log('<chatComponent.js renderChatPanel> {Object.keys(this.state.user) = ', Object.keys(this.state.user))
+  }
+  componentAlreadyRendered (userName) {
+    return Object
+              .values(this.state.user)
+              .filter(user => user === userName)
+              .length
   }
 
   renderPanel (key) {
-    console.log('<chatComponent.js renderPanel > key = ', key, ' user = ', this.state.user[key])
     return (
-      <ChatPannel key={key} index={key} details={{ key: key, user: this.state.user[key] }} />
+      <ChatPannel key={key} index={key} details={{ key: key, userName: this.state.user[key] }} />
     )
   }
   render () {
     return (
-      <div>
+      <div className='sideBar'>
         <ChatSideBar func_renderChatPanel={this.renderChatPanel} />
+        <div className='pannelMain'>
         {Object.keys(this.state.user).map(this.renderPanel)}
+        </div>
+        <style jsx>{`
+          .pannelMain {
+            display:flex;
+
+          }
+          .sideBar {
+            display: flex;
+            flex-direction: row-reverse;
+          }
+        `}</style>
       </div>
     )
   }
